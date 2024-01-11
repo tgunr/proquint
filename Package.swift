4de205fe75a4ec950b1b7fd762932085cd9dd3d8
Dave Carlton <davec@mac.com>
2024-01-11 02:28:57 -0600

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "Proquint",
+    products: [
+        .library(
+            name: "Proquint",
+            targets: ["Proquint"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "Proquint",
+            path: "Sources"
+        ),
+    ]
+)
